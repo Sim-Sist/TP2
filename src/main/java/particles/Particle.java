7391147d5a247212caf50c5d22a19dca3310fab1
aBenve <abenvenuto@itba.edu.ar>
2022-03-24 18:42:38 -0300

    Velocity and Acceleration added to Particle class. Update and Move methods too.

--- src/main/java/particles/Particle.java
+++ src/main/java/particles/Particle.java
@@ -1,5 +1,10 @@
 package particles;
 
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Set;
+
 public class Particle {
     private int index;
     public double x, y, vx, vy, ax, ay;
@@ -10,11 +15,33 @@ public class Particle {
         this.index = index;
         this.x = x;
         this.y = y;
+
+        double angle = Math.atan2(vy, vx);
+        this.vx = VELOCITY_MAGNITUDE * Math.cos(angle) + vx;
+        this.vy = VELOCITY_MAGNITUDE * Math.sin(angle) + vy;
+
         this.vx = vx;
         this.vy = vy;
         this.ax = ax;
         this.ay = ay;
         this.radius = radius;
+    }
+
+    public void move(List<Particle> particles, int particleCount) {
+        if (particleCount == 0)
+            return;
+
+        double meanAngle = 0;
+        for (Particle other : particles) {
+            meanAngle += Math.atan2(other.getVy(), other.getVx());
+        }
+
+        meanAngle /= particleCount;
+
+        this.vx = VELOCITY_MAGNITUDE * Math.cos(meanAngle) + this.vx;
+        this.vy = VELOCITY_MAGNITUDE * Math.sin(meanAngle) + this.vy;
+
+        this.update();
     }
 
     public void update() {
@@ -28,6 +55,21 @@ public class Particle {
 
     public int getIndex() {
         return index;
+    }
+
+    public List<Double> getVelocity() {
+        List<Double> velocity = new ArrayList<>();
+        velocity.add(this.vx);
+        velocity.add(this.vy);
+        return velocity;
+    }
+
+    public double getVx() {
+        return vx;
+    }
+
+    public double getVy() {
+        return vy;
     }
 
     public double distanceTo(Particle p) {
