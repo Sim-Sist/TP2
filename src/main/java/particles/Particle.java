c446d3ec9eebfbc72302663f8218d96c32e2c4fc
Agustin <agalarza@itba.edu.ar>
2022-03-28 11:02:15 -0300

    Added random noise to particle movement

--- src/main/java/particles/Particle.java
+++ src/main/java/particles/Particle.java
@@ -1,7 +1,7 @@
 package particles;
 
 import java.util.List;
-import java.util.ArrayList;
+import java.util.Random;
 
 public class Particle {
     private int index;
@@ -60,6 +60,16 @@ public class Particle {
         this.update();
     }
 
+    private static double randomFloat(double minInclusive, double maxInclusive) {
+        double precision = 0.00001;
+        int max = (int) (maxInclusive / precision);
+        int min = (int) (minInclusive / precision);
+        Random rand = new Random();
+        int randomInt = rand.nextInt((max - min) + 1) + min;
+        double randomNum = randomInt * precision;
+        return randomNum;
+    }
+
     public void move(List<Double> neighbourAngles, double noise) {
         int particleCount = neighbourAngles.size();
         this.update();
@@ -69,7 +79,7 @@ public class Particle {
         double sinAvg = neighbourAngles.stream().mapToDouble(a -> Math.sin(a)).average().getAsDouble();
         double cosAvg = neighbourAngles.stream().mapToDouble(a -> Math.cos(a)).average().getAsDouble();
 
-            this.speedAngle = Math.atan2(sinAvg, cosAvg);
+        this.speedAngle = Math.atan2(sinAvg, cosAvg) + Particle.randomFloat(-noise / 2, noise / 2);
 
     }
 
