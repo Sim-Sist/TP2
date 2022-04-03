89785da0922dee02ee9cc26813b3a7baf0e48d87
Agustin <agalarza@itba.edu.ar>
2022-04-03 19:21:46 -0300

    improved use for polarization_analysis.py

--- src/main/python/polarization_analysis.py
+++ src/main/python/polarization_analysis.py
@@ -8,6 +8,7 @@ import os
 from statistics import mean,stdev
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pnd
 
 PYTHON_LOCAL_PATH = "/main/python/"
 OUTPUT_LOCAL_PATH = "/main/output/"
@@ -16,11 +17,15 @@ PLOTS_LOCAL_PATH = OUTPUT_LOCAL_PATH + "plots/"
 
 ####################### Main
 
-SIM_AMOUNT = 20
 FRAMES_PER_SIM = 2000
 VELOCITY = 0.3
+variable_values = [5]
+# variable_values.extend([(i+1)*0.5 for i in range(20)])
+SIM_AMOUNT = len(variable_values)
+print(SIM_AMOUNT)
 
 NAME_OF_VARIABLE="Density"
+PLOT_EXTENSION="png" # png or pdf (better quality)
 
 mean_polarizations = []
 deviations = []
@@ -55,7 +60,7 @@ plt.errorbar([i * 0.5 for i in range(len(mean_polarizations))], mean_polarizatio
 plt.ylim(0,1)
 plt.xlim(0,5)
 plt.grid(axis='both', alpha=.3)
-plt.xticks([i * 0.5 for i in range(len(mean_polarizations))],[f'{i * 0.5:g}' for i in range(len(mean_polarizations))])
+plt.xticks([i * 0.5 for i in range(len(mean_polarizations))],[f'{i:g}' for i in variable_values])
 plt.yticks([i * 0.1 for i in range(11)])
 ax_label_fontsize = 15
 plt.xlabel(NAME_OF_VARIABLE, fontsize=ax_label_fontsize )
@@ -69,13 +74,13 @@ plt.gca().spines["left"].set_alpha(0.3)
 
 plt.title('Speed polarization through %s variation'%(NAME_OF_VARIABLE),fontsize=ax_label_fontsize*1.5)
 
-plt.savefig(plot_output_path+"%s.pdf"%(NAME_OF_VARIABLE),bbox_inches='tight')
+plt.savefig(plot_output_path+"%s.%s"%(NAME_OF_VARIABLE,PLOT_EXTENSION),bbox_inches='tight')
 
 for i in range(len(raw_polarizations)):
     fig = plt.figure(figsize=(20,13),dpi=100)
     yvals = raw_polarizations[i]
     xvals = [i for i in range(len(yvals))]
-    plt.title("Polarization for %s=%g"%(NAME_OF_VARIABLE,i*0.5), fontsize=ax_label_fontsize *1.5)
+    plt.title("Polarization for %s=%g"%(NAME_OF_VARIABLE,variable_values[i]), fontsize=ax_label_fontsize *1.5)
     plt.plot(xvals,yvals)
 
     plt.ylim(0,1)
@@ -92,8 +97,23 @@ for i in range(len(raw_polarizations)):
     plt.gca().spines["right"].set_alpha(0.0)
     plt.gca().spines["left"].set_alpha(0.3)
 
-    plot_name = "plot%02d.pdf"%(i)
+    plot_name = "plot%02d.%s"%(i,PLOT_EXTENSION)
     plt.savefig(plot_output_path+plot_name,bbox_inches='tight')
+    plt.clf()
+    plt.close('all')
+
+df = pnd.DataFrame(
+    {
+        "Density": [(i+1) * 0.5 for i in range(len(mean_polarizations))],
+        "Polarization":mean_polarizations,
+        "Error":deviations
+    })
+
+print(df)
 
 
+# print("Mean Polarizations:")
+# print("Density: polarization")
+# for i in range(len(mean_polarizations)):
+#     print("%d: %d"%((i+1)*0.5,mean_polarizations[i]))
 
